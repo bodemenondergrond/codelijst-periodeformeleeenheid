723b673f82b55552ba9b0db99a6e1850a5fbb201
gezever <gezever@skynet.be>
2024-07-29 15:46:23 +0200

    javascript

--- src/main/bash/01_csv_to_rdf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-Rscript ../R/csv_to_rdf.R
-xsltproc ../xsl/rdf_to_xsd.xsl ../resources/be/vlaanderen/bodemenondergrond/data/id/conceptscheme/periodeformeleeenheid/periodeformeleeenheid.rdf > ../resources/be/vlaanderen/bodemenondergrond/data/id/conceptscheme/periodeformeleeenheid/periodeformeleeenheid.xsd
